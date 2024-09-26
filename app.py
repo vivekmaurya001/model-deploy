85eaf4c0dc055cd5e1ec36b7d225f5319d9fc7cb
vivekmaurya001 <vm8118134@gmail.com>
2024-09-26 21:23:45 +0530

    new commit21

--- app.py
+++ app.py
@@ -1,39 +1,31 @@
 from flask import Flask, request, jsonify
-import util
+import util  # Make sure this is imported correctly
+
 app = Flask(__name__)
 
-@app.route('/')
+# Load saved artifacts when the app starts
+util.load_saved_artifacts()
+
+@app.route('/hello')
 def hello():
     return "HI"
 
-
 @app.route('/predict_stock_price', methods=['POST'])
 def predict_stock_price():
-    company= request.form['company']
-    country= request.form['country']
-    year= float(request.form['year'])
+    company = request.form['company']
+    country = request.form['country']
+    year = float(request.form['year'])
     market_cap = float(request.form['market_cap'])
     expenses = float(request.form['expenses'])
     revenue = float(request.form['revenue'])
     market_share = float(request.form['market_share'])
 
     response = jsonify({
-        'estimated_stock_price': util.get_predicted_stock(company,country,year,market_cap,expenses,revenue,market_share)
+        'estimated_stock_price': util.get_predicted_stock(company, country, year, market_cap, expenses, revenue, market_share)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
 if __name__ == "__main__":
-    print("Starting Python Flask Server For Home Price Prediction...")
-    util.load_saved_artifacts()
-    app.run(host='0.0.0.0', port=5000)
-
-# >> # Create a new virtual environment
-# >> virtualenv venv
-# >>
-# >> # Activate the environment
-# >> # On Windows
-# >> venv\Scripts\activate
-# pip install flask numpy pandas   
-# pip install gunicorn pickle json
+    print("Starting Python Flask Server For Stock Price Prediction...")
+    app.run()
